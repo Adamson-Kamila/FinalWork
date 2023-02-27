58e1b8017c92c2c5ba4c2dbd45eb3333ce4fb7da
Kamila <kamilla.adamson@gmail.com>
2023-02-27 23:28:54 +0600

    Added a Task

---
+++ Task/Program.cs
@@ -0,0 +1,49 @@
+﻿Console.Clear();
+
+//Для существующего массива, выполняем следующие действия.
+//Сперва программа пробегается по готовому массиву, чтобы определиь количество элементов, 
+//удовлетворяющих требованиям (количество символов в элементе должно быть меньше или равно 3).
+
+string[] array = new string[] { "hello", "2", "world", ":-)" };
+
+int getArray(string[] array) // функция по подсчету элементов, удовлетворяющих
+//условию в заданном массиве.
+{
+  int count = 0;
+
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (array[i].Length <= 3)
+    {
+      count += 1;
+    }
+  }
+  return count;
+}
+
+//Console.WriteLine(getArray(array));
+
+
+void resultArray(int count) // в функци resultArray создается новый массив, который заполняется
+// значениями, удовлетворяющими условия задачи.
+{
+  string[] newArray = new string[count];
+  int j = 0;
+
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (array[i].Length <= 3)
+    {
+      newArray[j] = array[i];
+      j += 1;
+    }
+  }
+
+  var str = string.Join(" ", newArray);
+  Console.Write(str);
+
+}
+
+resultArray(getArray(array)); //int x = getArray(array);
+                              //resultArray(x);
+
